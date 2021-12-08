12c761bb4b07df00b0903d1f9551ac0c091d2626
Florian Tanner <58835636+FCTanner@users.noreply.github.com>
2021-12-08 14:47:57 +0100

    Solve P1

--- 2021/Day 4 Giant Squid/day_4.R
+++ 2021/Day 4 Giant Squid/day_4.R
@@ -1,12 +1,6 @@
 rm(list = ls())
 .libPaths("C:/R-packages2/")
 
-library(tidyverse)
-
-# Read first line line for called numbers
-# Create list of matrices for the boards
-
-
 # Parse called numbers ----------------------------------------------------
 
 called_numbers <- read.csv("input.txt", header = FALSE, nrows = 1)
@@ -15,12 +9,9 @@ called_numbers <- as.numeric(matrix(called_numbers[1,]))
 
 # Parse boards ------------------------------------------------------------
 
+boards <- read.table(file= "input.txt", sep = "" , header = F, skip= 2)
+
 board_list <- list()
-
-
-full_data <- read_fwf("input.txt",  fwf_widths(rep(3, 5)), skip_empty_rows = TRUE)
-boards <- full_data[1:nrow(full_data),] |> 
-  filter(!is.na(X1))
 
 for(i in 1:nrow(boards)){
   if(i %% 5 == 0){
@@ -32,12 +23,53 @@ for(i in 1:nrow(boards)){
   }
 }
 
+# Part 1 ------------------------------------------------------------------
 
 # Iterate over boards and "eliminate" numbers -----------------------------
-length(board_list)
 
-for(i in 1:length(board_list)){
-  # print(board_list[[i]])
+replace_winners <- function(board, number){
+  board[board==number] <- 0
+  board
+}
+
+check_if_bingo <- function(board){
+  row_sums = rowSums(board)
+  col_sums = colSums(board)
+  if(sum(row_sums == 0) > 0 |
+     sum(col_sums == 0) > 0){
+    # print("Bingo!")
+    stop("Bingo!")
+    return(TRUE)
+  }else{
+    # print("No winner yet")
+    return(FALSE)
+  }
+}
+
+
+for(w in 1:length(called_numbers)){
+  winning_number <- called_numbers[w]
+  # print(winning_number)
+  for(b in 1:length(board_list)){
+    board_list[[b]] <- replace_winners(board = board_list[[b]], 
+                                       number = winning_number)
+    last_number <- winning_number
+    check_if_bingo(board_list[[b]])
+  }
   
 }
+
+
+# Sum of all unmarked numbers ---------------------------------------------
+
+sum_unmarked <- sum(rowSums(board_list[[b]])) 
+
+# Solve 1 -------------------------------------------------------------------
+
+sum_unmarked * last_number
+
+
+
+# Part 2 ------------------------------------------------------------------
+
 
